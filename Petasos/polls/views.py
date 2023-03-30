c742f7d25c1f81989ab5ed0559cd5d71f8a92b3f
dm1681 <dm1681@gmail.com>
2023-03-29 18:18:25 -0700

    testing examples

--- Petasos/polls/views.py
+++ Petasos/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -16,12 +17,20 @@ class IndexView(generic.ListView):
         """
         returns the last five published questions.
         """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that arent published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
